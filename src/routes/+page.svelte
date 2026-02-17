5ea7efe53ac0533ef6a41de7c5a178c32f6a7d7d
Alexis Garcia Saez <lexisgasa@uoc.edu>
2026-02-17 10:51:18 +0100

    feature: add initial layout and styling

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main
+  class="min-h-screen bg-slate-900 flex flex-col items-center py-20 px-4 gap-12"
+>
+  <h1 class="text-3xl sm:text-4xl font-extrabold text-cyan-400">Es gurú?</h1>
+
+  <div class="text-base sm:text-xl text-slate-300">
+    <p class="mb-2">
+      Sospechas que el post que acabas de leer está escrito por un gurú de la
+      IA?
+    </p>
+    <p>Comprueba si el post tiene información relevante o es una cuñadez.</p>
+  </div>
+
+  <textarea
+    class="w-full max-w-2xl h-44 p-6 rounded-xl border border-slate-600 focus:border-cyan-400 focus:ring-cyan-400 bg-slate-800 text-slate-300 placeholder:text-slate-300 text-base sm:text-xl"
+    placeholder="Pega el texto aquí"
+  ></textarea>
+</main>
